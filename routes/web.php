c75327b1cd31ab0577952d9caf7d990366a5d80e
SmileNP <smile.bestybay@nabepero.co.jp>
2023-08-13 19:30:08 -0700

    create posts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function (){
+    $user = User::findOrFail(1);
+    $post = new Post(['title'=>'First post with Edwin', 'body'=>'Laravel LOVe']);
+    $user->posts()->save($post);
 });
